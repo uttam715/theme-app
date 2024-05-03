e3366479ef7b0d7667f4e3ef0546e25259b6592d
uttam715 <kumariuttam715@gmail.com>
2024-05-03 13:27:48 +0530

    changing theme using useref

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { useState } from 'react';
-import './App.css';
-import Info from './info';
+import { useRef } from "react";
+import "./App.css";
+import Info from "./info";
 
 function App() {
-  const [theme,setTheme]=useState(false);
+  const themeRef = useRef(null);
+
+  function themeHandleClick() {
+    if (themeRef.current.classList.contains("info")) {
+      themeRef.current.classList.replace("info", "darktheme");
+    } else {
+      themeRef.current.classList.replace("darktheme", "info");
+    }
+  }
   return (
-    <div className='main'>
-      <div className={`${theme ?"darktheme":"info"} `}>
-        <div className='title'>Title
-        <span className='theme' onClick={()=>{
-          if(theme){
-            setTheme(false);
-          }
-          if(!theme){
-            setTheme(true);
-          }
-        }} >theme</span>
+    <div className="main">
+      <div className="info" ref={themeRef}>
+        <div className="title">
+          Title
+          <span className="theme" onClick={themeHandleClick}>
+            theme
+          </span>
         </div>
 
         <Info value="First Name"></Info>
         <Info value="Last Name"></Info>
-        <div className='btndiv'>
-        <button className='btn'>Submit</button>
-
+        <div className="btndiv">
+          <button className="btn">Submit</button>
         </div>
       </div>
-
     </div>
   );
 }
