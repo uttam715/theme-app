fc6c2f2fe19518fcbb3acacaaeaf420dcbdabb72
uttam715 <kumariuttam715@gmail.com>
2025-02-11 12:12:53 +0530

    first comit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import { useRef } from "react";
-import "./App.css";
-import Info from "./info";
-
 function App() {
-  const themeRef = useRef(null);
-
-  function themeHandleClick() {
-    if (themeRef.current.classList.contains("info")) {
-      themeRef.current.classList.replace("info", "darktheme");
-    } else {
-      themeRef.current.classList.replace("darktheme", "info");
-    }
-  }
-  return (
-    <div className="main">
-      <div className="info" ref={themeRef}>
-        <div className="title">
-          Title
-          <span className="theme" onClick={themeHandleClick}>
-            theme
-          </span>
-        </div>
-
-        <Info value="First Name"></Info>
-        <Info value="Last Name"></Info>
-        <div className="btndiv">
-          <button className="btn">Submit</button>
-        </div>
-      </div>
-    </div>
-  );
+return(
+  <div>Creating Todo Application</div>
+)
 }
 
 export default App;
